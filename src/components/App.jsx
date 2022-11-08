0adf6fd40e801243aa61f0f508656718500fb922
Valik Kinakh <valik.kinah@gmail.com>
2022-11-08 18:29:50 +0200

    add contact form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,24 @@
 import Title from './common/Title';
 import SubTitle from './common/SubTitle';
+import ContactForm from './ContactForm';
+import { useEffect, useState } from 'react';
 
 export const App = () => {
+  const [contacts, setContacts] = useState([]);
+
+  const addContact = contact => {
+    setContacts(prevState => [...prevState, contact]);
+  };
+
+  useEffect(() => {
+    console.log(contacts);
+  }, [contacts]);
+
   return (
     <div className="app">
       <Title title="Phonebook" />
+      <ContactForm addContact={addContact} />
+
       <SubTitle title="Contacts" />
     </div>
   );
